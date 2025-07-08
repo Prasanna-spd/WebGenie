4667e9d52951180839fdb85991d22e7e11c0e46e
Sarada Prasanna Dash <dashsharada@gmail.com>
2025-07-08 22:31:34 +0530

    changed template logic

--- src/app/templateStore/page.tsx
+++ src/app/templateStore/page.tsx
@@ -15,9 +15,12 @@ export default function TemplateStorePage() {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
         {templates.map((template) => (
-          <div key={template.id} onClick={() => router.push("/preview/ecomm")} className="bg-white relative text-black p-5 rounded-xl shadow hover:shadow-lg cursor-pointer transition-transform duration-300 hover:-translate-y-5">
-            {template.id !== "ecomm" && <div className="absolute w-full h-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 bg-gray-700/70 text-center text-white text-lg font-bold px-4 py-2 rounded shadow">
-            <p className="absolute w-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">Restricted · Template under progress</p></div>}
+          <div key={template.id} onClick={() => router.push(template.active === "true" ? template.route : "/preview/ecomm")} className="bg-white relative text-black p-5 rounded-xl shadow hover:shadow-lg cursor-pointer transition-transform duration-300 hover:-translate-y-5">
+            {template.active === "false" && (
+              <div className="absolute w-full h-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 bg-gray-700/70 text-center text-white text-lg font-bold px-4 py-2 rounded shadow">
+                <p className="absolute w-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">Restricted · Template under progress</p>
+              </div>
+            )}
 
             <div className="w-full h-48 relative rounded overflow-hidden mb-4">
               <Image src={template.image} alt={template.title} fill className="object-cover" />
--- src/lib/template.ts
+++ src/lib/template.ts
@@ -1,60 +1,67 @@
 export interface TemplateMeta {
-    id: string;
-    title: string;
-    desc: string;
-    image: string;
-    route: string; // for preview
-  }
-  
-  export const templates: TemplateMeta[] = [
-    {
-      id: "ecomm",
-      title: "Ecom Express",
-      desc: "A stylish and powerful template designed to kickstart your ecommerce journey with elegance and speed.",
-      image: "/ecomm.png",
-      route: "/preview/ecomm",
-    },
-    {
-      id: "gym1",
-      title: "Power Pulse",
-      desc: "Bold and energetic design perfect for gyms, personal trainers, and fitness businesses.",
-      image: "/gymming.png",
-      route: "/preview/gym",
-    },
-    {
-      id: "portfolio",
-      title: "Showcase Your Work..",
-      desc: "A personal portfolio to display your creativity, skills, and achievements.Welcome to portfolio. This space is thoughtfully designed ........",
-      image: "/portfolio.png",
-      route: "/preview/portfolio",
-    },
-    {
-      id: "saas",
-      title: "LaunchPad SaaS",
+  id: string;
+  title: string;
+  desc: string;
+  image: string;
+  route: string; // for preview
+  active: string;
+}
+
+export const templates: TemplateMeta[] = [
+  {
+    id: "ecomm",
+    title: "Ecom Express",
+    desc: "A stylish and powerful template designed to kickstart your ecommerce journey with elegance and speed.",
+    image: "/ecomm.png",
+    route: "/preview/ecomm",
+    active: "true",
+  },
+  {
+    id: "gym1",
+    title: "Power Pulse",
+    desc: "Bold and energetic design perfect for gyms, personal trainers, and fitness businesses.",
+    image: "/gymming.png",
+    route: "/preview/gym",
+    active: "true",
+  },
+  {
+    id: "portfolio",
+    title: "Showcase Your Work..",
+    desc: "A personal portfolio to display your creativity, skills, and achievements.Welcome to portfolio. This space is thoughtfully designed ........",
+    image: "/portfolio.png",
+    route: "/preview/portfolio",
+    active: "false",
+  },
+  {
+    id: "saas",
+    title: "LaunchPad SaaS",
     desc: "Conversion-optimized landing page for SaaS startups and product launches.",
     image: "/saas.png",
     route: "/preview/saas",
-    },
-    {
-      id: "restaurant",
-      title: "DineSpot",
+    active: "false",
+  },
+  {
+    id: "restaurant",
+    title: "DineSpot",
     desc: "A flavorful restaurant template featuring menu highlights, booking, and location details.",
     image: "/restaurant.png",
     route: "/preview/restaurant",
-    },
-    {
-      id: "realestate1",
-      title: "HomeQuest",
-      desc: "Professional and elegant layout for real estate agents and property listings.",
-      image: "/realestate.png",
-      route: "/preview/realestate",
-    },
-    {
-      id: "agency1",
-      title: "ConsultEdge",
-      desc: "Corporate and clean template perfect for consultancies, agencies, and business service firms.",
-      image: "/agency.png",
-      route: "/preview/agency",
-    },
-  ];
-  
+    active: "false",
+  },
+  {
+    id: "realestate1",
+    title: "HomeQuest",
+    desc: "Professional and elegant layout for real estate agents and property listings.",
+    image: "/realestate.png",
+    route: "/preview/realestate",
+    active: "false",
+  },
+  {
+    id: "agency1",
+    title: "ConsultEdge",
+    desc: "Corporate and clean template perfect for consultancies, agencies, and business service firms.",
+    image: "/agency.png",
+    route: "/preview/agency",
+    active: "false",
+  },
+];
