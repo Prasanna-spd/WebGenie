343ec6e240feb63f61865cd288a21f9588e3b70e
Sarada Prasanna Dash <dashsharada@gmail.com>
2025-07-14 23:31:48 +0530

    solved page issue for mobile view

--- src/components/Vision.tsx
+++ src/components/Vision.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Vision() {
   return (
-    <div id="vision" className="relative text-[#0a0a68] bg-[#97a4ff] h-screen flex items-center justify-center px-4">
+    <div id="vision" className=" text-[#0a0a68] bg-[#97a4ff] h-screen flex items-center justify-center px-4">
       <div className="text-6xl  md:text-8xl font-extralight flex items-center justify-center md:w-[1152px] w-[404px] text-center ">
         <h2>
           <span> Your vision. Your goals. Your website.</span>
